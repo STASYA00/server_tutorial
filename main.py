c3a6daa5738b395df0830012341d5af38ab39d0c
STASYA00 <0.0stasya@gmail.com>
2025-05-15 18:10:16 +0200

    feat: update files

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask("test server")
+app = Flask("test server", static_url_path="/static", static_folder="static")
+
+db = {
+    "Karin": {
+        "age": 23,
+        "height": 1.70
+    },
+    "John": {
+        "age": 25,
+        "height": 1.80
+    }
+}
 
 
 """
 www.learn.knivkit.com/
 """
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -31,7 +41,7 @@ def show_model(model):
     www.learn.knivkit.com/models/building234
     """
     print(model)
-    return f'Model input: {model}'
+    return {"content": f'Model input: {model}'}
 
 @app.route("/post_model", methods=["POST"])
 def post_model():
@@ -44,6 +54,34 @@ def post_model():
     print(my_input)
     return my_input["a"]
 
+@app.route("/update_db", methods=["POST"])
+def update_db():
+    """
+
+    requests.post(url="http://172.22.203.223:3000/post_model", json={"a": "b"})
+
+    """
+    my_input = request.get_json()
+    print(my_input)
+    db.update(my_input)
+    print(db)
+    return my_input
+
+
+@app.route("/template/<name>")
+def template(name):
+    user_info = db.get(name)
+    return render_template("1.html", info=user_info)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
+
+
+
+
+
+# @app.route("/template")
+# def template():
+#     n = {"name": 234}
+#     return render_template("1.html", name="Karin")
